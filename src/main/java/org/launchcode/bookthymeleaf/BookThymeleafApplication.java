f588d329344c98233f1312823ebd587400b0eef9
pdmxdd <pdmxdd@gmail.com>
2021-07-13 12:25:21 -0500

    starter

---
+++ src/main/java/org/launchcode/bookthymeleaf/BookThymeleafApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.bookthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/bookthymeleaf/BookThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.bookthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
